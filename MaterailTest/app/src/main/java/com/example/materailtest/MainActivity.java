e416e92ad44ea7d575f29595f4756bf167886d08
mqzswa <1403882232@qq.com>
2022-08-30 15:45:32 +0800

    DrawerLayout  CoordinatorLayout  Toolbar FloatingActionButton    NavigationView

--- MaterailTest/app/src/main/java/com/example/materailtest/MainActivity.java
+++ MaterailTest/app/src/main/java/com/example/materailtest/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.materailtest;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        NavigationView navigationView=findViewById(R.id.navigation_view);
+
+        navigationView.setCheckedItem(R.id.nav_Call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        FloatingActionButton floatingActionButton=findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "you clicked fab", Toast.LENGTH_SHORT).show();
+
+                Snackbar snackbar=Snackbar.make(v,"Delete data",Snackbar.LENGTH_LONG);
+                snackbar.setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"Data restore",Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+            case R.id.is_backup:
+                Toast.makeText(this , "You clicked backup", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.is_delete:
+                Toast.makeText(this, "You clicked delete", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+
+            case R.id.is_settings:
+                Toast.makeText(this, "You clicked setting", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+        return true;
     }
 }
